ad5d2196c14730c8689c9bdc90fba96210172fdc
DannyCastroVentura <dannycastroventura@gmail.com>
2021-10-03 16:11:58 +0100

    Adicionar dockerfile

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.engine('html', require('ejs').renderFile);
 
-app.listen(3000, ()=> {
-    console.log("Server started on port 3000");
+app.listen(3001, ()=> {
+    console.log("Server started on port 3001");
 });
 
 app.get("/", (req, res) => {
